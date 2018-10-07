799e067d4edcd2b4fc8177782202703aa279ca4a
john kevin <johnkevinbarrera@gmail.com>
2018-10-07 03:30:51 -0500

    db

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ var port= process.env.PORT || 3789;
 mongoose.Promise = global.Promise;
 
 //Conexion a la base de datos Mongo
-mongoose.connect('mongodb://localhost:27017/zoo', { useNewUrlParser: true })
+//mongoose.connect('mongodb://localhost:27017/zoo', { useNewUrlParser: true })
+
+mongoose.connect('mongodb://admin123:admin123@ds157964.mlab.com:57964/mydbjkn', { useNewUrlParser: true })
 	.then( ()=>{
 		console.log('La conexion a la base de datos se ha realizado correctamente...');
 		app.listen(port,()=>{
 			console.log("El servidos local con Node y Expres esta corriendo correctamente ...");
-		})	
+		})
 	})
 	.catch(err => console.log(err));
